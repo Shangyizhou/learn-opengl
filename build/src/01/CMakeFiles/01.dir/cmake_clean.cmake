8b046845ec582e35ba4707898147c6beddd48814
shangyizhou <2503194262@qq.com>
2024-10-27 16:13:10 +0800

    create opengl project

---
+++ build/src/01/CMakeFiles/01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01.dir/main.cc.obj"
+  "CMakeFiles/01.dir/main.cc.obj.d"
+  "D:/shangyizhou/code/opengl/learn_opengl/bin/01.exe"
+  "D:/shangyizhou/code/opengl/learn_opengl/bin/01.exe.manifest"
+  "D:/shangyizhou/code/opengl/learn_opengl/bin/01.pdb"
+  "D:/shangyizhou/code/opengl/learn_opengl/bin/lib01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/01/CMakeFiles/demo01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo01.dir/main.cc.obj"
+  "CMakeFiles/demo01.dir/main.cc.obj.d"
+  "D:/shangyizhou/code/opengl/learn_opengl/bin/demo01.exe"
+  "D:/shangyizhou/code/opengl/learn_opengl/bin/demo01.exe.manifest"
+  "D:/shangyizhou/code/opengl/learn_opengl/bin/demo01.pdb"
+  "D:/shangyizhou/code/opengl/learn_opengl/bin/libdemo01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
